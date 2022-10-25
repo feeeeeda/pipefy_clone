e742439dc234a87e919bb9d4f22db4f324ad9c9b
Fernanda Guerra <fguerrafgr@gmail.com>
2022-10-25 11:14:35 -0300

    testing components

---
+++ src/components/Card/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Container, Label } from './styles';
+
+
+export default function Card() {
+  return (
+    <Container>
+      <header>
+        <Label color="#7159c1" />
+      </header>
+      <p>Fazer a migração completa de servidor</p>
+      <img src="https://i.pinimg.com/236x/a1/9d/fd/a19dfd0cd5b84390c983b372c91b7f22.jpg" alt="" />
+    </Container>
+  )
+}
